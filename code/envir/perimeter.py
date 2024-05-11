2d3b8c6757bcfda3ffdba3c0ca2d21c2d71938e0
szl <thuszl784@163.com>
2024-05-11 18:53:31 +0800

    1) 增加边界交叉口完整信号配时方案的计算; 2）增加在边界交叉口设置完整SUMO信号方案功能

--- code/envir/perimeter.py
+++ code/envir/perimeter.py
@@ -1,14 +1,20 @@
-import sumolib
 import traci
 import numpy as np
 from utils.utilize import config
-from collections import deque
+from peritsc.perimeterdata import PeriSignals
+from peritsc.signal_controller import PeriSignalController
+from typing import Dict
 
 # EdgeCross = [33034, 53054]
 
 
 class Peri_Agent():
-    def __init__(self, tsc_peri):
+    def __init__(self, tsc_peri, peridata: PeriSignals):
+        # control mode
+        self.distribution_mode = config['peri_distribution_mode']
+        self.signal_phase_mode = config['peri_signal_phase_mode']
+        self.optimization_mode = config['peri_optimization_mode']
+
         # signal constraints
         self.min_green = config['min_green']
         self.max_green = config['max_green']
@@ -16,15 +22,18 @@ class Peri_Agent():
         self.cycle_time = config['cycle_time']
         self.downedge_maxveh = config['max_queue']
 
-        # perimiter infos
+        # perimeter infos
         self.info = config['Peri_info']
         self.peri_num = len(self.info)
         self.splitmode = config['splitmode']  # 0 = waittime, 1 = waitveh
         self.peri_action_mode = config['peri_action_mode']
         self.flowrate = config['flow_rate']  # 1 veh /cross time
         self.tsc_peri = tsc_peri
+        self.peridata = peridata
         # list to record datas
         self.green_time = np.array([])
+        self.inflow_movements = []
+
         # self.entered_veh = np.array([])
 
     def get_waitfeature(self):
@@ -46,16 +55,17 @@ class Peri_Agent():
         # return wait_feature
 
     def get_greensplit(self, action, steps):
-        """ disctribute the vehicles on the boundary with green split
+        """
+        distribute the vehicles on perimeter nodes with green split
         """
         green_time = np.zeros(len(self.info))
 
         if self.peri_action_mode == 'centralize':
             ############ consider upstream waiting features
-            if False: 
+            if False:
                 # First, get wait feature
                 self.get_waitfeature()
-                
+
                 # Secend, calcualte green split
                 ### waitfeature i\
                 # s veh number
@@ -75,12 +85,12 @@ class Peri_Agent():
                 green_time = (
                     permit_vehnum *
                     self.flowrate).round()  # transfer: veh num --> green time
-            
-            ############# directly split green time given total inflow vehicles 
+
+            ############# directly split green time given total inflow vehicles
             elif False:
                 green_time[:] = (action * self.flowrate / len(self.info)).round()
-            
-            ############# directly split green time given total green time        
+
+            ############# directly split green time given total green time
             else:
                 green_time[:] = round((action / len(self.info)), 0)
 
@@ -92,7 +102,7 @@ class Peri_Agent():
         green_time = np.maximum(green_time, self.min_green)
         # green_time = np.maximum(green_time, 0)
 
-        red_time = self.cycle_time - green_time - 2* self.yellow_duration
+        red_time = self.cycle_time - green_time - 2 * self.yellow_duration
 
         self.green_time = np.reshape(np.append(self.green_time, green_time), [-1, len(self.info)])
         # print(self.green_time)
@@ -102,6 +112,61 @@ class Peri_Agent():
 
         return green_time, red_time
         # print(f"action of greentime :{green_time}")
+
+    def get_full_greensplit(self, action, peri_state: Dict[str, Dict[str, list]]):
+        """
+        distribute the vehicles on perimeter nodes with green split
+        """
+        # 1. Collect critical parameters (demand & initial queue)
+        for signal_id, signal in self.peridata.peri_signals.items():
+            for lane_id, lane in signal.in_lanes.items():
+                if len(peri_state):
+                    # calculate the real queue length at the implementation step
+                    # assume that the queue length reaches maximum at the end of red
+                    halting_veh_evolution = peri_state['halting'][lane_id]
+                    entered_veh_evolution = peri_state['entered'][lane_id]
+                    left_veh_evolution = peri_state['left'][lane_id]
+                    # print(f'The evolution of halting vehicles on lane {lane_id}: {halting_veh_evolution}')
+                    # print(f'The evolution of entered vehicles on lane {lane_id}: {entered_veh_evolution}')
+                    # print(f'The evolution of left vehicles on lane {lane_id}: {left_veh_evolution}')
+                    red_end_step = halting_veh_evolution.index(max(halting_veh_evolution))
+                    total_newly_entered_veh = sum(entered_veh_evolution[red_end_step:])     # 假设一个cycle内到达率不变
+                    total_newly_left_veh = sum(left_veh_evolution[red_end_step:])
+                    flow = sum(entered_veh_evolution) / self.cycle_time     # veh/s
+                    queue = max(halting_veh_evolution) + total_newly_entered_veh - total_newly_left_veh
+                    queue = max(queue, 0)
+                else:   # step = 0
+                    density = traci.lane.getLastStepVehicleNumber(lane_id) / lane.length
+                    speed = traci.lane.getLastStepMeanSpeed(lane_id)
+                    flow = density * speed      # veh/s
+                    queue = traci.lane.getLastStepHaltingNumber(lane_id)
+                avg_vehicle_length = traci.lane.getLastStepLength(lane_id)
+                lane.set_arrival_rate(flow)
+                lane.set_queue(queue)
+                lane.set_vehicle_length(avg_vehicle_length)
+            for movement_id, movement in signal.movements.items():
+                downlink_id = movement.down_link.id
+                queue = traci.edge.getLastStepHaltingNumber(downlink_id)
+                signal.downLinks[downlink_id].update_state(queue)
+
+        # 2. Optimize the signal plan of all perimeter intersections
+        controller = PeriSignalController(self.peridata, action)
+        estimate_inflow = controller.signal_optimize()
+
+        # # centralized method
+        # # set_inflow_local_green(peri_data=self.peridata, required_total_inflow=action)
+        #
+        # # decentralized method
+        # set_inflow_green(peri_data=self.peridata, required_total_inflow=action)
+        # set_local_green(peri_data=self.peridata)
+
+        # 3. record green time data
+        inflow_movement_green_time = self.peridata.get_inflow_green_duration()
+        self.inflow_movements = list(inflow_movement_green_time.keys())
+        self.green_time = np.reshape(np.append(self.green_time, list(inflow_movement_green_time.values())),
+                                     [-1, len(inflow_movement_green_time)])
+
+        return estimate_inflow
 
     def set_program(self, green_time, red_time):
         for peri_id, peri in dict.items(self.info):
@@ -115,6 +180,49 @@ class Peri_Agent():
             ## set program
             traci.trafficlight.setProgramLogic(peri_id, logic) # set the new program
             # print(traci.trafficlight.getAllProgramLogics(peri_id))
+
+    def set_full_program(self):
+
+        signal_plans = self.peridata.plan2program()
+        # self.peridata.print_program()
+        for signal_id, signal_plan in signal_plans.items():
+            phases = []
+            for state, duration in signal_plan.items():
+                phase = traci.trafficlight.Phase(duration, state)
+                phases.append(phase)
+
+            tsc = self.info[signal_id]['tsc']
+            logic = tsc.logic
+            # if logic.programID == '0':
+            #     logic.programID = '1'
+            logic.phases = tuple(phases)
+            traci.trafficlight.setProgramLogic(signal_id, logic)
+            # traci.trafficlight.setProgram(signal_id, logic.programID)
+            traci.trafficlight.setPhase(signal_id, 0)
+
+        # for peri_id, peri in dict.items(self.info):
+        #     peri_edge = peri['edge']
+        #     peri_approaches = peri['in_edges']
+        #     gated_edge_idx = peri_approaches.index(peri_edge)
+        #
+        #     tsc = peri['tsc']
+        #     logic = tsc.logic
+        #     signal_plan = signal_plan_info[peri_id]
+        #     if signal_plan == -1:
+        #         print(f'Node {peri_id} failed to get optimal signal plan. ')
+        #         traci.trafficlight.setProgramLogic(peri_id, logic)  # use current program
+        #     else:
+        #         signal_program = plan2program(cycle=config['cycle_time'], green_start=signal_plan[0],
+        #                                       green_duration=signal_plan[1], clearance_time=config['yellow_duration'],
+        #                                       gated_idx=gated_edge_idx)
+        #         # set signal program
+        #         phases = []
+        #         for state, duration in signal_program.items():
+        #             phase = traci.trafficlight.Phase(duration, state)
+        #             phases.append(phase)
+        #         logic.phases = tuple(phases)
+        #         traci.trafficlight.setProgramLogic(peri_id, logic) # set the new program
+
 
 
     def switch_phase(self, steps):
